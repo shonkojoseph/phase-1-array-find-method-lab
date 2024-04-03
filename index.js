460d6491b0a03f082fc521f8ed24e6cb4eb164d6
Joseph <josephshonko99@gmail.com>
2024-04-03 12:34:41 +0300

    commiting find method

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record) {add
+    const winningRecord = record.find(game => game.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+}
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+];
+
+console.log(superbowlWin(record));
